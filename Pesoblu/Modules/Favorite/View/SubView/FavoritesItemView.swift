4d21bde94383213e60944168718b181802713be9
Daniel Carcacha <80797075+danielcarc@users.noreply.github.com>
2025-08-22 12:04:07 -0300

    Se eliminaron comentarios y líneas de código comentadas sin uso, dejando únicamente el contenido relevante de la vista de favoritos

--- Pesoblu/Modules/Favorite/View/SubView/FavoritesItemView.swift
+++ Pesoblu/Modules/Favorite/View/SubView/FavoritesItemView.swift
@@ -1,5 +1,5 @@
 //
-//  FavoritesView.swift
+//  FavoritesItemView.swift
 //  Pesoblu
 //
 //  Created by Daniel Carcacha on 16/07/2025.
@@ -9,8 +9,6 @@ import SwiftUI
 import Kingfisher
 
 struct FavoritesItemView: View {
-
-    ///numero de denuncia N` 252664
     
     let place : PlaceItem
     let onTap: () -> Void
@@ -41,19 +39,15 @@ struct FavoritesItemView: View {
             VStack(alignment: .leading, spacing: 4){
                 Text(place.title ?? "")
                     .font(.headline)
-                    //.frame(maxWidth: .infinity, alignment: .leading)
                 Text(place.subtitle ?? "")
                     .foregroundColor(.gray)
                     .font(.subheadline)
-                    //.frame(maxWidth: .infinity, alignment: .leading)
                 Text(place.price ?? "")
                     .foregroundColor(.gray)
                     .font(.subheadline)
-                  //  .frame(maxWidth: .infinity, alignment: .leading)
                 Text(place.distance ?? "0.0km")
                     .foregroundStyle(Color.gray)
                     .font(.subheadline)
-                   // .frame(maxWidth: .infinity, alignment: .leading)
             }
             .padding(.horizontal, 8)
             .padding(.bottom, 8)
@@ -61,7 +55,6 @@ struct FavoritesItemView: View {
         .background(Color.white)
         .cornerRadius(16)
         .shadow(color: Color.black.opacity(0.08), radius: 6, x: 0, y: 3)
-        //.padding(.horizontal)///si agregabamos este hay mas espacio horizontal y el collectionview queda mas chico de ancho
         .padding(.vertical, 4)
     }
 }
